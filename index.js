9e95b1157af62b64c284dee45797acf3876fb47b
Marcos Silva <marcos.201314@hotmail.com>
2025-09-25 15:44:32 -0300

    feat: adicionando funcoes de download e atualizando o log

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
 const path = require("path");
+const archiver = require("archiver");
+const { pipeline } = require("stream/promises");
+const { log } = require("console");
 
 const PORT = 3000;
 const TARGET_URL = "http://omnissolucoes.com/teste3/";
@@ -14,6 +17,7 @@ const TARGET_URL = "http://omnissolucoes.com/teste3/";
 */
 
 const LOG_FILE = path.join(__dirname, "error.log");
+const DOWNLOAD_DIR = path.join(__dirname, "downloads");
 
 const app = express();
 
@@ -57,6 +61,49 @@ async function extractFiles(url) {
   }
 }
 
+async function downloadFile(file) {
+  try {
+    const url = new URL(file.fullUrl, TARGET_URL).toString();
+    const filePath = path.join(DOWNLOAD_DIR, path.basename(file.fullUrl));
+
+    const response = await axios.get(url, {
+      responseType: "stream",
+      timeout: 10000,
+    });
+    await pipeline(response.data, fs.createWriteStream(filePath));
+
+    return filePath;
+  } catch (err) {
+    const status = err.response?.status;
+    if (status === 404 || status === 403) {
+      logError(`Arquivo não encontrado: ${file.fullUrl} (${status})`);
+    } else {
+      logError(`Erro ao baixar ${file.fullUrl}: ${err.message}`);
+    }
+    return null;
+  }
+}
+
+async function createZip(files) {
+  return new Promise((resolve, reject) => {
+    const zipPath = path.join(DOWNLOAD_DIR, "arquivos.zip");
+    const output = fs.createWriteStream(zipPath);
+    const archive = archiver("zip", { zlib: { level: 9 } });
+
+    output.on("close", () => resolve(zipPath));
+    archive.on("error", reject);
+
+    archive.pipe(output);
+
+    files.forEach((filePath) => {
+      const name = path.basename(filePath);
+      archive.file(filePath, { name });
+    });
+
+    archive.finalize();
+  });
+}
+
 (async () => {
   try {
     const files = await extractFiles(TARGET_URL);
@@ -66,6 +113,33 @@ async function extractFiles(url) {
     console.error(err.message);
   }
 })();
+
+app.get("/download", async (req, res) => {
+  try {
+    const filesToDownload = await extractFiles(TARGET_URL);
+
+    const downloadedFiles = [];
+
+    for (const file of filesToDownload) {
+      const filePath = await downloadFile(file);
+
+      if (filePath) downloadedFiles.push(filePath);
+    }
+
+    if (downloadedFiles.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Nenhum arquivo disponível para download." });
+    }
+
+    const zipPath = await createZip(downloadedFiles);
+    res.download(zipPath, "arquivos.zip");
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({ error: err.message });
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Servidor rodando em http://localhost:${PORT}`)
